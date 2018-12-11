9a0368de5e9b247b33581a34d7f1f5dc290ad9fe
samerbam <unconfigured@null.spigotmc.org>
2018-12-11 17:51:46 -0500

    Ukulele

--- js/init.js
+++ js/init.js
@@ -1,7 +1,20 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.sidenav').sidenav();
+		$('.sidenav').sidenav();
 
-  }); // end of document ready
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+	$('.collapsible').collapsible();
+
+$('input.autocomplete').autocomplete({
+	data: {
+		"Apple": null,
+		"Microsoft": null,
+		"Google": 'https://placehold.it/250x250'
+	},
+});
+
+});
